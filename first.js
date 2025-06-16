d7a07970f7271a620ae16ad8078b26da4346840d
Dharmpal Gurjar <dhgurjar2002@gmail.com>
2025-06-16 09:22:51 +0530

    Payment Added

--- first.js
+++ first.js
@@ -1,3 +1,12 @@
 const button = "Added a Button " ;
 
 console.log(button)
+
+
+const login = "Login page added";
+console.log(login);
+
+
+const Payment = "Integrated the payment gateway";
+
+console.log(Payment)
